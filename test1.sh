b426dd38ea8b607204ea323a9962bd5e1c6a6b45
Maitri <maitri@york.ie>
2024-02-21 06:46:59 +0000

    added another random line

---
+++ test1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Add all changes
+git add .
+
+# Get the commit message from the last commit in the Gerrit branch
+commitMessage=$(git log --format=%B -n 1 master)
+
+# Get the name of the Gerrit branch
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+# Commit changes with the same message as the last commit in the Gerrit branch
+git commit -m "$commitMessage"
+
+# Push changes to the same branch in the GitHub repository
+git push origin "$branch"
+
+# Optional: Pause the script before exiting
+read -rp "Press Enter to exit..."
+
+#Hello 
